46e99c93c77695f22c083d515b2ea1529e3d61b2
marcoskeller@hotmail.com <marcoskeller@hotmail.com>
2020-10-13 11:01:39 -0300

    Realizado toda estrutura para Deletar vendedor.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
@@ -25,7 +26,18 @@ namespace SalesWebMvc.Services
         {
             _context.Add(obj);
             _context.SaveChanges();
-        }  
+        } 
+        
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
 
     }
 }
