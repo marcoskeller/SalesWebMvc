eb1fb036dd8d20c4ab13261ed94f95c79ef5fcf5
marcoskeller@hotmail.com <marcoskeller@hotmail.com>
2020-10-14 14:22:30 -0300

    Realizado s vriacao do Tratamento de exceção para delete (integridade referencial).

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -34,9 +34,17 @@ namespace SalesWebMvc.Services
         }
         public async Task RemoveAsync(int id)
         {
-            var obj =await  _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                //throw new IntegrityException(e.Message);
+                throw new IntegrityException("Não é possível deletar um vendedor que já tenho realizado vendas.");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
