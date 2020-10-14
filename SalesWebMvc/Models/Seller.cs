986975c0b17fec698a6241d32e0c22a7a19d9b31
marcoskeller@hotmail.com <marcoskeller@hotmail.com>
2020-10-14 10:36:48 -0300

    Realizado a implementacao da Localização e formatação de números e datas.

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -10,10 +11,16 @@ namespace SalesWebMvc.Models
 
         public string  Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-
+        
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:.00}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
